f6fa6e8c7fbbb7865f31dd5d6a9378f8aa37d7db
theNewJson <jason5540105@gmail.com>
2018-06-24 19:03:49 +0800

    add immer to solve immutable state probelm(unsolved)

--- src/reducers/locationReducer.js
+++ src/reducers/locationReducer.js
@@ -1,5 +1,6 @@
 // locationReducer.js
 import * as actionType from '../actions/ActionType';
+import produce from "immer";
 
 const today = new Date();
 const initialState = {
@@ -16,43 +17,45 @@ const initialState = {
 };
 const locationReducer = (state = initialState, action) => {
   let newState = {...state};
-  switch (action.type) {
-    case actionType.UPDATE_FOCUS_DAY:
-      newState.focusDay = action.payload;
-      newState.day[state.focusDay].isFocus = false;
-      newState.day[newState.focusDay].isFocus = true;
-      console.log(newState);
-
-      return newState;
-    case actionType.ADD_LOCATION:
-      // if(newState[newState.focusDay] === undefined){
-      //   newState[newState.focusDay] = {date:new Date(),location:[]};
-      // }
-      newState.day[newState.focusDay].location.push(action.payload);
-      return newState;
-    case actionType.UPDATE_TRIP_INTERVAL:
-      newState.startDate = action.payload.startDate;
-      newState.endDate = action.payload.endDate;
-      let startDate = new Date(newState.startDate);
-      let endDate = new Date(newState.endDate);
-      //console.log((endDate-startDate)/(24*3600*1000) + 1);
-      let tmpDay = {};
-      var i;
-      for(i = 0 ; i < ((endDate-startDate)/(24*3600*1000) + 1) ; i++){
-        let tmpDate = new Date(newState.startDate);
-        let dayID = i+1;
-        tmpDate.setDate(startDate.getDate()+i);
-        let prevLocation = newState.day[dayID] !== undefined ? newState.day[dayID] : {location: []};
-        tmpDay[dayID] = {...prevLocation,date: new Date(tmpDate), isFocus:false};
-      }
-      newState.day = tmpDay;
-      newState.focusDay = i;
-      newState.day[i].isFocus = true;
-      console.log(newState);
-      return newState;
-    default:
-      return state;
-  }
+  let nextState;
+  nextState = produce(state, draftState => {
+    switch (action.type) {
+      case actionType.UPDATE_FOCUS_DAY:
+        draftState.focusDay = action.payload;
+        draftState.day[state.focusDay].isFocus = false;
+        draftState.day[draftState.focusDay].isFocus = true;
+        break;
+      case actionType.ADD_LOCATION:
+        draftState.day[draftState.focusDay].location.push(action.payload);
+        break;
+      case actionType.UPDATE_TRIP_INTERVAL:
+        draftState.startDate = action.payload.startDate;
+        draftState.endDate = action.payload.endDate;
+        let startDate = new Date(draftState.startDate);
+        let endDate = new Date(draftState.endDate);
+        //console.log((endDate-startDate)/(24*3600*1000) + 1);
+        let tmpDay = {};
+        var i;
+        for(i = 0 ; i < ((endDate-startDate)/(24*3600*1000) + 1) ; i++){
+          let tmpDate = new Date(draftState.startDate);
+          let dayID = i+1;
+          tmpDate.setDate(startDate.getDate()+i);
+          if(draftState.day[dayID] === undefined){
+            draftState.day[dayID] = {location: []};
+          }
+          draftState.date = new Date(tmpDate);
+          draftState.isFocus = false;
+          // let prevLocation = draftState.day[dayID] !== undefined ? draftState.day[dayID] : {location: []};
+          // tmpDay[dayID] = {...prevLocation,date: new Date(tmpDate), isFocus:false};
+        }
+        // draftState.day = tmpDay;
+        draftState.focusDay = 1;
+        draftState.day[1].isFocus = true;
+        // console.log(draftState.day[1].location === state.day[1].location);
+    }
+  });
+  console.log(state.day[1].location === nextState.day[1].location);
+  return nextState;
 }
 
 export default locationReducer;
